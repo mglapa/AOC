1dc96bf7c31682fb75a5de9bc70605be8b734838
MICHAEL GLAPA <MGLAPA@MICRON.COM>
2021-11-30 21:41:58 -0800

    added template file

---
+++ 2021/template.py
@@ -0,0 +1,48 @@
+
+def load():
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
+    return lines
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    
+    lines = load()
+
+    part1(lines)
+
+    part2(lines)
+
+
+if __name__ == '__main__':
+    main()    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
